b5184f3a966a7021f3bec34cd6fa690b229b294d
i-rishikesh <risikeshmishra2102@gmail.com>
2024-07-02 01:04:45 +0530

    added tailwind

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,4 @@
-<h1>Hello</h1>
+<h1>Welcome to SvelteKit</h1>
+<p class="ml-20 pt-20 pl-20 hover:underline font-bold hover:text-red-500">
+  Visit<a href="http://kit.svelte.dev">kit.sveltle.dev</a>
+</p>
